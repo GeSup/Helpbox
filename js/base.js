1e969b19a0606e0138a4468b5cefe03dcbab0c65
GeSup <skuter6@interia.pl>
2019-11-17 14:40:42 +0100

    baza osobno

--- js/base.js
+++ js/base.js
@@ -2,3 +2,19 @@
  var config = {
  };
  firebase.initializeApp(config);
+ const auth = firebase.auth();
+ const db = firebase.firestore();
+
+ // update firestore settings
+ db.settings({
+     timestampsInSnapshots: true
+ });
+ // Get the modal
+ var modal = document.getElementById('modal-signup');
+
+ // When the user clicks anywhere outside of the modal, close it
+ window.onclick = function(event) {
+     if (event.target == modal) {
+         modal.style.display = "none";
+     }
+ }
