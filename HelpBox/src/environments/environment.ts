d68565ade48e1c9e22d02bc74a9da5e9aad3d4dc
GeSup <skuter6@interia.pl>
2019-12-23 16:51:26 +0100

    Add login angular version

--- HelpBox/src/environments/environment.ts
+++ HelpBox/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  config: {
+    apiKey: "AIzaSyDh0Hz4P6f6YRxQMrTXAMWwnKcB5jYhkWI",
+    authDomain: "chatbox-4b9dc.firebaseapp.com",
+    databaseURL: "https://chatbox-4b9dc.firebaseio.com",
+    projectId: "chatbox-4b9dc",
+    storageBucket: "chatbox-4b9dc.appspot.com",
+    messagingSenderId: "96696942261",
+    appId: "1:96696942261:web:887d37a3129e8074f008fc",
+    measurementId: "G-WPEZK9RGWZ"}
 };
 
 /*
