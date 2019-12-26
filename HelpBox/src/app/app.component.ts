fc22301618d2c1de63985d40f84dcd70b57a7993
GeSup <skuter6@interia.pl>
2019-12-26 16:49:36 +0100

    add click events

--- HelpBox/src/app/app.component.ts
+++ HelpBox/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
+  
   title = 'HelpBox';
+  public changeView: boolean = true;
+ 
+  public toggle():void{ this.changeView = !this.changeView;}
+
+  changeHide(val: boolean) {
+    this.changeView = val;
+  }
 }
