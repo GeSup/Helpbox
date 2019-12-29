1e1db5de0824aec245b35c56673424d943ad98a8
GeSup <skuter6@interia.pl>
2019-12-29 15:29:44 +0100

    new fix

--- HelpBox/src/app/auth/register/register.component.ts
+++ HelpBox/src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -9,14 +10,29 @@ export class RegisterComponent implements OnInit {
 
   @Output() onHide = new EventEmitter<boolean>();
 
-  constructor() { }
+  constructor(private authService:AuthService) { }
 
   ngOnInit() {
   }
-
+  email: string;
+  password: string;
+  confirmPassword: string;
+  dontMatch: string;
 
   setHide(){
     this.onHide.emit(true);
+  }
+
+  signUp() {
+    if (this.password === this.confirmPassword){
+    this.authService.SignUp(this.email, this.password);
+    this.email = '';
+    this.password = '';
+    this.confirmPassword = '';
+    this.dontMatch = '';
+    } else {
+      this.dontMatch = 'Passwords Don\'t Match';
+    }
   }
   
 
